9c5f612fc190baa4fc2fbb404a44aa6345005dd0
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2021-07-21 11:40:00 +0300

    Add processAccountRStoHex function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { BigInteger } from 'jsbn';
 import * as ApolloApi from './apollo-api-v2/api';
 import Transaction from './Transaction';
 import Crypto from './Crypto';
@@ -10,6 +11,12 @@ export const processAccountIDtoRS = (accountID: number | string) => {
 
 export const processAccountRStoID = (accountRS: string) => {
   return ReedSolomonDecode(accountRS);
+};
+
+export const processAccountRStoHex = (accountRS: string, needPrefix: boolean = false) => {
+  const accountID = ReedSolomonDecode(accountRS);
+  const hexString = new BigInteger(accountID).toString(16);
+  return needPrefix ? `0x${hexString}` : hexString;
 };
 
 export { Transaction, Crypto, ElGamalEncryption, ApolloApi };
